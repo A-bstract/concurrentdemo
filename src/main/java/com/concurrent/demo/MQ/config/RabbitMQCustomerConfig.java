ce435b6128d1df889cca718120d409353ff60401
a_bstract <15710563453@163.com>
2019-08-05 18:05:40 +0800

    去掉多余引用 看着不舒服

--- src/main/java/com/concurrent/demo/MQ/config/RabbitMQCustomerConfig.java
+++ src/main/java/com/concurrent/demo/MQ/config/RabbitMQCustomerConfig.java
@@ -1,23 +1,15 @@
 package com.concurrent.demo.MQ.config;
 
-import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AcknowledgeMode;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.handler.annotation.Headers;
 
-import java.io.IOException;
-import java.util.Map;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
