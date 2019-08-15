d193e7da267354056aaea8ce8c31b5999bcfee21
A-bstract <15710563453@163.com>
2019-08-15 22:25:53 +0800

    线程中断测试

---
+++ src/test/java/com/concurrent/demo/T1/interrupt/ThreadInterruptLockTest.java
@@ -0,0 +1,11 @@
+package com.concurrent.demo.T1.interrupt;
+
+/**
+ * 线程中断测试 - 锁
+ */
+public class ThreadInterruptLockTest {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/concurrent/demo/T1/interrupt/ThreadIterruptTest.java
@@ -0,0 +1,51 @@
+package com.concurrent.demo.T1.interrupt;
+
+/**
+ * 线程中断测试 - 常规
+ */
+public class ThreadIterruptTest {
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new MRunnable());
+        t1.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t1.interrupt();
+    }
+
+    public static class MRunnable implements Runnable{
+        @Override
+        public void run() {
+            System.out.println("子线程启动！");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                //抛出异常以后  中断标识会被清空 因为线程为了处理异常已经重新处于就绪状态
+                System.out.println(Thread.currentThread().isInterrupted());
+                //再将它执为中断
+                Thread.currentThread().interrupt();
+                //这回为true了
+                System.out.println(Thread.currentThread().isInterrupted());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e1) {
+                    System.out.println("线程阻塞 倒了catch");
+                }
+                //InterruptedException检查异常会自动清除中断状态
+                System.out.println(Thread.currentThread().isInterrupted());
+            }
+
+            //让该线程继续工作
+            System.out.println("继续工作...");
+            //再次中断
+            Thread.currentThread().interrupt();
+            //这个静态方法会清除中断标记（副作用）
+            System.out.println(Thread.interrupted());
+            //返回false
+            System.out.println(Thread.currentThread().isInterrupted());
+        }
+    }
+}
