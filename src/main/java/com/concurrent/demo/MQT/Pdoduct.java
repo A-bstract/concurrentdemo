e164a6ea05be2af19e71bea1803bfc5dd22a034a
A-bstract <15710563453@163.com>
2019-08-17 20:18:22 +0800

    线程中断测试

--- src/main/java/com/concurrent/demo/MQT/Pdoduct.java
+++
@@ -1,9 +0,0 @@
-package com.concurrent.demo.MQT;
-
-import com.bld365.mq.core.annotation.MQTemplateConfiguration;
-import com.bld365.mq.core.template.MQTemplate;
-
-@MQTemplateConfiguration(group="${mq.producer.group}", nameServer="${mq.name-server}")
-public class Pdoduct {
-
-}
